91c390acceb63813cf76352243b50977df3d6b95
DESKTOP-9TD1TJ7\sunda <sunday_n@naver.com>
2020-06-18 23:29:16 +0900

    - 프로젝트 재생성

--- src/main/java/com/bp/msa/MsaApplication.java
+++ src/main/java/com/bp/msa/MsaApplication.java
@@ -2,12 +2,12 @@ package com.bp.msa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MsaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsaApplication.class, args);
     }
-
 }
